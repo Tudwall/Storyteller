2a1c915fda302ee61df8192ae5234f065711c04f
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-07-12 14:29:56 +0200

    add devServer

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 module.exports = {
     devtool: 'source-map',
+    devServer: {
+        contentBase: "./dist",
+    },
     entry: './src/js/index.js',
     output: {
         filename: 'main.js',
