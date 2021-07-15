554d3f5040e10a8865ead597ae9ec38964ea7603
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-07-15 17:00:43 +0200

    create quizComponent factory function

---
+++ src/js/quiz-component.js
@@ -0,0 +1,25 @@
+const quizComponent = (quizInfo) => {
+    const question = document.createElement("p");
+    question.id = "question";
+    question.textContent = quizInfo.question;
+
+    const submitBtn = document.createElement("button");
+    submitBtn.id = "submit-btn";
+    submitBtn.textContent = "Submit my answer!";
+    submitBtn.addEventListener("click", quizClickHandler);
+
+    const quiz = document.createElement("div");
+    quiz.id = "quiz";
+    quiz.appendChild(question);
+
+    const choices = quizInfo.choices;
+    choices.forEach(choice => {
+        const answer = document.createElement("input");
+        answer.setAttribute("type", "radio");
+        answer.id = `data-${indexOf(choice)}`;
+        answer.textContent = choice;
+        quiz.appendChild(answer);
+    });
+
+    quiz.appendChild(submitBtn);
+}
