653511e4b1654ccee2d8dea2c9a61f6dbeb4f023
Emmy <emerentia.arjani@gmail.com>
2021-07-17 20:07:00 +0700

    added changes to chapterEnd

--- src/js/chapterEnd.js
+++ src/js/chapterEnd.js
@@ -5,29 +5,23 @@ const createChapterEnd = (chapterNumber, quiz, nextPage, homePage) => {
     const base = document.createElement("div");
     base.setAttribute('id', 'chapter-end')
 
-    let quizPassed = false;
-    let toggleQuizStatus = () => quizPassed = true; 
-
     let text = document.createElement("p");
-    text.innerHTML = `Chapter ${chapterNumber} complete!`
+    text.textContent = `Chapter ${chapterNumber} complete!`
 
     let nextButton = document.createElement("button");
-    nextButton.innerHTML = `Next Chapter`;
-    //if quiz has not been passed yet, do NOT display next chapter button
-    nextButton.style.display = (quizPassed)? "block" : "none";
+    nextButton.textContent = `Next Chapter`;
     nextButton.addEventListener("click", () => {
         //renderContent(nextPage)
     })
 
     let homeButton = document.createElement("button");
-    homeButton.innerHTML = `Back To Home`;
+    homeButton.textContent = `Back To Home`;
     homeButton.addEventListener("click", () => {
         //renderContent(homePage)
     })
 
     base.appendChild(text);
-    //Need to implement quiz.
-    // base.appendChild(createQuiz(quizInfo, toggleQuizStatus));
+    //need to implement quiz later. 
     base.appendChild(nextButton);
     base.appendChild(restartButton);
 
