463d79d65d92be0c03d3bafac55052123cb6f799
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-07-17 15:43:24 +0200

    create a form and labels, add a title

--- src/js/quiz-component.js
+++ src/js/quiz-component.js
@@ -1,4 +1,8 @@
 const quizComponent = (quizInfo) => {
+    const title = document.createElement("h2");
+    title.id = "title";
+    title.textContent = "Pop quiz!";
+  
     const question = document.createElement("p");
     question.id = "question";
     question.textContent = quizInfo.question;
@@ -6,22 +10,37 @@ const quizComponent = (quizInfo) => {
     const submitBtn = document.createElement("button");
     submitBtn.id = "submit-btn";
     submitBtn.textContent = "Submit my answer!";
-    submitBtn.addEventListener("click", quizClickHandler);
+
+    // submitBtn.addEventListener("click", answerHandler);
 
     const quiz = document.createElement("div");
     quiz.id = "quiz";
-    quiz.appendChild(question);
+    quiz.appendChild(title);
+  
+    const form = document.createElement("form");
+    form.id = "answers";
+  
+    form.appendChild(question); 
 
     const choices = quizInfo.choices;
     choices.forEach(choice => {
         const answer = document.createElement("input");
         answer.setAttribute("type", "radio");
-        answer.setAttribute("data-i", indexOf(choice));
-        answer.textContent = choice;
-        quiz.appendChild(answer);
+        answer.id = `choice-${choices.indexOf(choice)}`;
+        answer.setAttribute("name", "answer");
+        answer.setAttribute("data-i", choices.indexOf(choice));
+
+        const label = document.createElement("label");
+        label.setAttribute("for", `${choice}`);
+        label.textContent = choice;
+        
+        form.appendChild(answer);
+        form.appendChild(label);
     });
 
-    quiz.appendChild(submitBtn);
+    form.appendChild(submitBtn);
+    quiz.appendChild(form);
+   
 
     return quiz
 }
