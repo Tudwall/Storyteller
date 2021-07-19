3b76a792b70fb056bc173a3f0af58fa65e7110d2
Michał Długosz <mdugosz@gmail.com>
2021-07-19 20:22:03 +0200

    Setup demo story

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,34 @@
 import "../styles/index.css";
+import { startingPage } from "./starting-page";
+import { firstChapter, secondChapter, thirdChapter } from "./demo-story";
+import { createChapterStructure } from "./chapterDOM";
+import { render } from "./render";
+
+//Now it's just one story object, later we should create factory for stories
+const storyObj = {
+  chapters: [],
+  addChapter(...chapterData) {
+    chapterData.forEach((data) => {
+      this.chapters.push(data);
+    });
+  },
+
+  findNextChapter(currentChapter) {
+    const chapterNumber = currentChapter.getChapterNumber();
+
+    return this.chapters.find(
+      (nextChapter) => nextChapter.getChapterNumber() === chapterNumber + 1
+    );
+  },
+};
+
+storyObj.addChapter(firstChapter, thirdChapter, secondChapter);
+
+const firstChapterStructure = createChapterStructure(firstChapter);
+const startPage = startingPage(() =>
+  render(firstChapterStructure.setupChapterPage())
+);
+
+render(startPage);
+
+export { storyObj };
