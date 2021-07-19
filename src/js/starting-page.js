069c71c25467cc7d0dbf48b6866031e083dae350
Michał Długosz <mdugosz@gmail.com>
2021-07-19 20:14:29 +0200

    Fix id assign bug, disable HtPBtn event listener

--- src/js/starting-page.js
+++ src/js/starting-page.js
@@ -1,31 +1,32 @@
 const startingPage = (renderFunction, HtPRenderFunction) => {
-    const title = document.createElement("h1");
-    title.id = "title";
-    title.textContent = "Unnamed Game";
+  const title = document.createElement("h1");
+  title.id = "title";
+  title.textContent = "Unnamed Game";
 
-    const desc = document.createElement("p");
-    desc.id = "description";
-    desc.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida consectetur est at pharetra. Cras quis arcu convallis magna egestas feugiat."
+  const desc = document.createElement("p");
+  desc.id = "description";
+  desc.textContent =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida consectetur est at pharetra. Cras quis arcu convallis magna egestas feugiat.";
 
-    const startGameBtn = document.createElement("button");
-    startGameBtn.id = "start-game-button";
-    startGameBtn.textContent = "Start game";
-    startGameBtn.addEventListener("click", renderFunction);
+  const startGameBtn = document.createElement("button");
+  startGameBtn.id = "start-game-button";
+  startGameBtn.textContent = "Start game";
+  startGameBtn.addEventListener("click", renderFunction);
 
-    const HtPBtn = document.createElement("button");
-    HtPBtn.id = "how-to-play-button";
-    HtPBtn.textContent = "How to play";
-    HtPBtn.addEventListener("click", HtPRenderFunction);
+  const HtPBtn = document.createElement("button");
+  HtPBtn.id = "how-to-play-button";
+  HtPBtn.textContent = "How to play";
+  //HtPBtn.addEventListener("click", HtPRenderFunction);
 
-    const mainMenu = document.createElement("div");
-    mainMenu.id("main-menu");
+  const mainMenu = document.createElement("div");
+  mainMenu.id = "main-menu";
 
-    mainMenu.appendChild(title);
-    mainMenu.appendChild(desc);
-    mainMenu.appendChild(startGameBtn);
-    mainMenu.appendChild(HtPBtn);
+  mainMenu.appendChild(title);
+  mainMenu.appendChild(desc);
+  mainMenu.appendChild(startGameBtn);
+  mainMenu.appendChild(HtPBtn);
 
-    return mainMenu
-}
+  return mainMenu;
+};
 
-export { startingPage }
+export { startingPage };
