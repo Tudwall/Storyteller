7a2836ec5ee233f9c9adaee6511af3c7b2b287bc
David Matalik <david.matalik@posteo.de>
2021-07-22 08:59:17 +0200

    Implement quiz-factory functionality

--- src/js/quiz-factory.js
+++ src/js/quiz-factory.js
@@ -1,5 +1,13 @@
-const Quiz = () => {
-  // Implement properties/functions
+const Quiz = (question, choices, answer) => {
+  let passed = false;
+
+  const getQuestion = () => question;
+  const getChoices = () => choices;
+  const getAnswer = () => answer;
+  const setPassed = () => (passed = !passed);
+  const getPassed = () => passed;
+
+  return { getQuestion, getChoices, getAnswer, setPassed, getPassed };
 };
 
 export default Quiz;
--- src/js/quiz-factory.test.js
+++ src/js/quiz-factory.test.js
@@ -5,18 +5,17 @@ let quiz1;
 beforeEach(() => {
   const question = "question1";
   const choices = ["choice1", "choice2", "choice3"];
-  const correctAnswerIndex = 1;
-  const passed = false;
+  const answer = 1;
 
-  quiz1 = Quiz(question, choices, correctAnswerIndex, passed);
+  quiz1 = Quiz(question, choices, answer);
 });
 
 test("getQuestion returns the set question", () => {
-  expect(quiz1.getQuestion()).toBe("question");
+  expect(quiz1.getQuestion()).toBe("question1");
 });
 
 test("getChoices returns the set choices", () => {
-  expect(quiz1.getChoices()).toBe(["choice1", "choice2", "choice3"]);
+  expect(quiz1.getChoices()).toStrictEqual(["choice1", "choice2", "choice3"]);
 });
 
 test("getAnswer returns the index of correct choice", () => {
