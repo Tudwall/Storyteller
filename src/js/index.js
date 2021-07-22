8ac6566b282a2d20da54f4553213e2fa719f3684
Michał Długosz <mdugosz@gmail.com>
2021-07-22 21:07:56 +0200

    Remove unecessary code

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,16 @@
 import "../styles/index.css";
 import { startingPage } from "./starting-page";
 import { firstChapter, secondChapter, thirdChapter } from "./demo-story";
-import { createChapterStructure } from "./chapterDOM";
 import { render } from "./render";
+import { gameLogic } from "./logic";
+import { Story } from "./stories-factory";
 
-//Now it's just one story object, later we should create factory for stories
-const storyObj = {
-  chapters: [],
-  addChapter(...chapterData) {
-    chapterData.forEach((data) => {
-      this.chapters.push(data);
-    });
-  },
+const firstStory = Story("Cool story");
 
-  findNextChapter(currentChapter) {
-    const chapterNumber = currentChapter.getChapterNumber();
+firstStory.addChapters(firstChapter, thirdChapter, secondChapter);
 
-    return this.chapters.find(
-      (nextChapter) => nextChapter.getChapterNumber() === chapterNumber + 1
-    );
-  },
-};
-
-storyObj.addChapter(firstChapter, thirdChapter, secondChapter);
-
-const firstChapterStructure = createChapterStructure(firstChapter);
-const startPage = startingPage(() =>
-  render(firstChapterStructure.setupChapterPage())
-);
+const storyLogic = gameLogic(firstStory);
+const storyStart = storyLogic.startFirstChapter();
+const startPage = startingPage(() => render(storyStart));
 
 render(startPage);
-
-export { storyObj };
