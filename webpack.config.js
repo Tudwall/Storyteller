d001f0774bf55b6a71ec4b80631e474b99e002c8
Emmy <emerentia.arjani@gmail.com>
2021-07-22 15:04:36 +0700

    added config for images

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,11 @@ module.exports = {
                 'style-loader',
                 'css-loader'
             ]
-            }
+            },
+            {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+            },
         ]
     }
 };
