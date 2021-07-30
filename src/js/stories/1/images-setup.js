6ee323cd4fd5cb8c33a3774d94063c70fea9b095
Michał Długosz <mdugosz@gmail.com>
2021-07-30 23:52:16 +0200

    Setup images for first story

---
+++ src/js/stories/1/images-setup.js
@@ -0,0 +1,47 @@
+const chapterOneImages = [
+  { cssClass: "drop-container", url: "../../images/dnd/1.1/trees.svg" },
+  { cssClass: "right", url: "../../images/dnd/1.1/tress.svg" },
+  { cssClass: "wrong", url: "../../images/dnd/1.1/building.svg" },
+  { cssClass: "wrong", url: "../../images/dnd/1.1/house-red.svg" },
+  { cssClass: "wrong", url: "../../images/dnd/1.1/pond.svg" },
+];
+
+const chapterTwoImages = [
+  { cssClass: "drop-container", url: "../../images/dnd/1.2/newspaper.svg" },
+  { cssClass: "right", url: "../../images/dnd/1.2/newspaper.svg" },
+  { cssClass: "wrong", url: "../../images/dnd/1.2/paper.svg" },
+  { cssClass: "wrong", url: "../../images/dnd/1.2/fabric.svg" },
+  { cssClass: "wrong", url: "../../images/dnd/1.2/book.svg" },
+];
+
+const chapterThreeImages = [
+  { cssClass: "drop-container", url: "../../images/dnd/1.3/kite-white.svg" },
+  { cssClass: "right", url: "../../images/dnd/1.3/kite-white.svg" },
+  { cssClass: "wrong", url: "../../images/dnd/1.3/kite-rainbow.svg" },
+  { cssClass: "wrong", url: "../../images/dnd/1.3/kite-orange.svg" },
+  { cssClass: "wrong", url: "../../images/dnd/1.3/kite-blue.svg" },
+];
+
+const chapterFourImages = [
+  { cssClass: "drop-container", url: "../../images/dnd/1.4/house-red.svg" },
+  { cssClass: "right", url: "../../images/dnd/1.4/house-red.svg" },
+  { cssClass: "wrong", url: "../../images/dnd/1.4/building-pink.svg" },
+  { cssClass: "wrong", url: "../../images/dnd/1.4/hospital.svg" },
+  { cssClass: "wrong", url: "../../images/dnd/1.4/house-blue.svg" },
+];
+
+const chapterFiveImages = [
+  { cssClass: "drop-container", url: "../../images/dnd/1.5/ladder.svg" },
+  { cssClass: "right", url: "../../images/dnd/1.5/ladder.svg" },
+  { cssClass: "wrong", url: "../../images/dnd/1.5/hammer.svg" },
+  { cssClass: "wrong", url: "../../images/dnd/1.5/rope.svg" },
+  { cssClass: "wrong", url: "../../images/dnd/1.5/stairs.svg" },
+];
+
+export {
+  chapterOneImages,
+  chapterTwoImages,
+  chapterThreeImages,
+  chapterFourImages,
+  chapterFiveImages,
+};
