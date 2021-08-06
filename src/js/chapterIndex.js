407a22e7188069e73df3315c46fd71962ae03f5e
Michał Długosz <mdugosz@gmail.com>
2021-08-06 17:07:59 +0200

    Fix status display

--- src/js/chapterIndex.js
+++ src/js/chapterIndex.js
@@ -1,106 +1,107 @@
-import { render } from "./render.js"
+import { render } from "./render.js";
 import { startPage } from "./index";
 import { createChapterStructure } from "./chapterDOM.js";
 
 const chapterIndex = (story, storyLogic) => {
-    let currentChapterNumber = 1;
-    let latestChapterNumber = findLatestCompletedChapter();
-    
-    const base = document.createElement("div");
-    base.id = "chapter-index";
+  let currentChapterNumber = 1;
+  let latestChapterNumber = findLatestCompletedChapter();
 
-    const text = document.createElement("h1");
-    text.textContent = "Chapters";
+  const base = document.createElement("div");
+  base.id = "chapter-index";
 
-    const home_button = document.createElement("button");
-    home_button.textContent = "Back to Home";
-    home_button.id = "home-button";
-    home_button.addEventListener("click", () => {
-        render(startPage);
-    })
+  const text = document.createElement("h1");
+  text.textContent = "Chapters";
 
-    const chaptersContainer = document.createElement("div");
-    chaptersContainer.id = "chapters-container";
+  const home_button = document.createElement("button");
+  home_button.textContent = "Back to Home";
+  home_button.id = "home-button";
+  home_button.addEventListener("click", () => {
+    render(startPage);
+  });
 
-    function findLatestCompletedChapter() {
+  const chaptersContainer = document.createElement("div");
+  chaptersContainer.id = "chapters-container";
 
-        let completedChapterExist = story.getChapters().some(chapter => {
-            return chapter.getCompletionStatus() == true;
-        });
+  function findLatestCompletedChapter() {
+    let completedChapterExist = story.getChapters().some((chapter) => {
+      return chapter.getCompletionStatus() == true;
+    });
 
-        //no chapter completed, start at 0 (only chapter 1 unlocked)
-        if (!completedChapterExist) {
-            return 0;
-        } else {
+    //no chapter completed, start at 0 (only chapter 1 unlocked)
+    if (!completedChapterExist) {
+      return 0;
+    } else {
+      let allChapters = story.getChapters();
+      let highestChapterNum = 1;
 
-            let allChapters = story.getChapters();
-            let highestChapterNum = 1;
-
-            //check all chapters in story, keep track of the highest completed chapter. 
-            for (let i=0; i < allChapters.length; i++) {
-
-                if (allChapters[i].getCompletionStatus() == true) {
-                    if (allChapters[i].getChapterNumber() > highestChapterNum) {
-                        //new highest completed chapter number found.
-                        highestChapterNum = allChapters[i].getChapterNumber();
-                    }
-                }
-            }
-            return highestChapterNum;
+      //check all chapters in story, keep track of the highest completed chapter.
+      for (let i = 0; i < allChapters.length; i++) {
+        if (allChapters[i].getCompletionStatus() == true) {
+          if (allChapters[i].getChapterNumber() > highestChapterNum) {
+            //new highest completed chapter number found.
+            highestChapterNum = allChapters[i].getChapterNumber();
+          }
         }
+      }
+      return highestChapterNum;
     }
+  }
 
-    /*Makes sure chapterNodes are appended in order, based on chapterNumber. Starts at 1, ends at last chapter 
+  /*Makes sure chapterNodes are appended in order, based on chapterNumber. Starts at 1, ends at last chapter 
     (or while loop breaks if it cannot find chapter with currentChapterNumber)
-    */    
+    */
 
-    while (story.getChapters().length !== currentChapterNumber-1) {
-        let chapter = story.getChapters().find(chapter => chapter.getChapterNumber() == currentChapterNumber);
+  while (story.getChapters().length !== currentChapterNumber - 1) {
+    let chapter = story
+      .getChapters()
+      .find((chapter) => chapter.getChapterNumber() == currentChapterNumber);
 
-        if (chapter == undefined) {
-            //there is a chapter missing. 
-            break;
-        } else {
+    if (chapter == undefined) {
+      //there is a chapter missing.
+      break;
+    } else {
+      const chapterNode = document.createElement("div");
+      chapterNode.className = "chapter";
 
-            const chapterNode = document.createElement("div");
-            chapterNode.className = "chapter";
+      const text = document.createElement("p");
+      text.textContent = `Chapter ${currentChapterNumber}`;
 
-            const text = document.createElement("p");
-            text.textContent = `Chapter ${currentChapterNumber} ${chapter.getCompletionStatus()}`;
+      const button = document.createElement("button");
+      button.textContent = "Play";
+      button.classList.add("play-chapter-button");
+      button.addEventListener("click", () => {
+        render(
+          createChapterStructure(chapter, () =>
+            storyLogic.displayMessage(chapter)
+          )
+        );
+      });
 
-            const button = document.createElement("button");
-            button.textContent = "Play";
-            button.classList.add("play-chapter-button");
-            button.addEventListener("click", () => {
-                render(createChapterStructure(chapter, () => storyLogic.displayMessage(chapter)));
-            });
+      // ONLY unlock "play" button for chapters up to latestChapterNumber (which is the last completed chapter) + 1";
+      if (currentChapterNumber <= latestChapterNumber + 1) {
+        //button enabled by default.
+      } else {
+        button.disabled = true;
+        button.textContent = "Locked";
+        button.classList.add("disabled-button");
+      }
 
-            // ONLY unlock "play" button for chapters up to latestChapterNumber (which is the last completed chapter) + 1";
-            if (currentChapterNumber <= latestChapterNumber + 1) {
-                //button enabled by default. 
-            } else {
-                button.disabled = true;
-                button.textContent = "Locked";
-                button.classList.add("disabled-button");
-            }
-        
-            chapterNode.appendChild(text);
-            chapterNode.appendChild(button);
+      chapterNode.appendChild(text);
+      chapterNode.appendChild(button);
 
-            chaptersContainer.appendChild(chapterNode);   
+      chaptersContainer.appendChild(chapterNode);
 
-            currentChapterNumber++;
-        }
+      currentChapterNumber++;
     }
+  }
 
-    console.log(latestChapterNumber);
+  console.log(latestChapterNumber);
 
-    base.appendChild(text);
-    base.appendChild(home_button);
-    base.appendChild(chaptersContainer);
+  base.appendChild(text);
+  base.appendChild(home_button);
+  base.appendChild(chaptersContainer);
 
-    return base;
+  return base;
 };
-
 
 export { chapterIndex };
